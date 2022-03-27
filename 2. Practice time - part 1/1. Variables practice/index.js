2dbb26b34ae38d5990627dc9dc3dfe53a0f8fcc6
Benjamin Ofori-Quansah <bquansah007@gmail.com>
2022-03-27 12:02:47 +0000

    practicing variables

--- 2. Practice time - part 1/1. Variables practice/index.js
+++ 2. Practice time - part 1/1. Variables practice/index.js
@@ -1,7 +1,9 @@
 // Create two variables, firstName and lastName
+let firstName = "Benjamin Essilfie";
+let lastName = "Ofori-Quansah";
 
 // Concatenate the two variables into a third variable called fullName
+let fullName = firstName + " " + lastName;
 
 // Log fullName to the console
-
-
+console.log(fullName);
