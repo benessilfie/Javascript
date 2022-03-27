6619276bc2de59bcfa2bf744a226f8c9a1954016
Benjamin Ofori-Quansah <bquansah007@gmail.com>
2022-03-27 12:15:45 +0000

    incr & decrement practice

--- 2. Practice time - part 1/3. Incrementing and decrementing/index.js
+++ 2. Practice time - part 1/3. Incrementing and decrementing/index.js
@@ -6,6 +6,7 @@ let myPoints = 3;
 function add3Points() {
   myPoints += 3;
 }
+
 add3Points();
 add3Points();
 add3Points();
@@ -13,6 +14,7 @@ add3Points();
 function remove1Point() {
   myPoints -= 1;
 }
+
 remove1Point();
 remove1Point();
 
