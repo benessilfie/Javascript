6fb2b9b47f56aa9bc19189949cca459d4e8e0579
Benjamin Ofori-Quansah <bquansah007@gmail.com>
2022-03-27 13:01:38 +0000

    building a calculator (practice test)

--- 2. Practice time - part 1/6. Calculator challenge/index.js
+++ 2. Practice time - part 1/6. Calculator challenge/index.js
@@ -1,10 +1,19 @@
 let num1 = 8;
 let num2 = 2;
-let sumEL = document.getElementById("sum-el");
 document.getElementById("num1-el").textContent = num1;
 document.getElementById("num2-el").textContent = num2;
 
 // Create four functions: add(), subtract(), divide(), multiply()
+
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+
+let sumEL = document.getElementById("sum-el");
+
 function add() {
   let sum = num1 + num2;
   sumEL.textContent = sum;
@@ -28,10 +37,3 @@ function multiply() {
   sumEL.textContent = sum;
   console.log(sum);
 }
-
-// Call the correct function when the user clicks on one of the buttons
-// Perform the given calculation using num1 and num2
-// Render the result of the calculation in the paragraph with id="sum-el"
-
-// E.g. if the user clicks on the "Plus" button, you should render
-// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
