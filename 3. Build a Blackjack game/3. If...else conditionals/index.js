483258fd53c6475cfb8f1e4beac63704d2141c37
Benjamin Ofori-Quansah <bquansah007@gmail.com>
2022-03-27 13:25:40 +0000

    my first if...else statements

--- 3. Build a Blackjack game/3. If...else conditionals/index.js
+++ 3. Build a Blackjack game/3. If...else conditionals/index.js
@@ -1,4 +1,11 @@
-let firstCard = 6
-let secondCard = 9
-let sum = firstCard + secondCard
+let firstCard = 6;
+let secondCard = 9;
+let sum = firstCard + secondCard;
 
+if (sum < 21) {
+  console.log("Do you want to draw another card?");
+} else if (sum === 21) {
+  console.log("Hurray! You've got Blackjack!");
+} else {
+  console.log("You are out of the game");
+}
