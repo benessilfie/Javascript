7d25aea3bd0987a36948d195336f90eeb46a93a8
Benjamin Ofori-Quansah <bquansah007@gmail.com>
2022-03-28 20:45:53 +0000

    working with arrays

--- 3. Build a Blackjack game/21. Intro to arrays/index.js
+++ 3. Build a Blackjack game/21. Intro to arrays/index.js
@@ -1,11 +1,17 @@
 // Arrays - ordered lists of items
 
 let featuredPosts = [
-    "Check out my Netflix clone",
-    "Here's the code for my project",
-    "I've just relaunched my portfolio"
-]
+  "Check out my Netflix clone",
+  "Here's the code for my project",
+  "I've just relaunched my portfolio",
+];
 
 // Create an array that lists your i.e. experience, education, licenses, skills or similar
 // The items of the array should be strings
 
+let education = [
+  "High School: St. Augustine's College",
+  "University: Ghana Communication Technology",
+  "University: AltSchool Africa",
+  "Other Schools: Holberton School",
+];
