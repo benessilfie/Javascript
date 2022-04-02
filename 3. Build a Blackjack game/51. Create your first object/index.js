ce3bc3723959d4849604e9686d9400f9721b8026
Benjamin Ofori-Quansah <bquansah007@gmail.com>
2022-04-02 07:09:45 +0000

    created an object

--- 3. Build a Blackjack game/51. Create your first object/index.js
+++ 3. Build a Blackjack game/51. Create your first object/index.js
@@ -2,4 +2,12 @@
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
 
+let airbnb = {
+  title: "Live like a king in my castle",
+  price: 190,
+  isSuperHost: true,
+  description: [4, 2, 2, 2],
+};
 
+console.log(airbnb.title);
+console.log(airbnb.price);
