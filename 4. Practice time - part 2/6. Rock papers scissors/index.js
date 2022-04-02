c5c9e2d92f36a35971293c9a70cb3fe592736a11
Benjamin Ofori-Quansah <bquansah007@gmail.com>
2022-04-02 19:28:28 +0000

    rock paper scissors practice

--- 4. Practice time - part 2/6. Rock papers scissors/index.js
+++ 4. Practice time - part 2/6. Rock papers scissors/index.js
@@ -1,5 +1,10 @@
-let hands = ["rock", "paper", "scissor"]
+let hands = ["rock", "paper", "scissor"];
 
 // Create a function that returns a random item from the array
 
+function rockPaperScissors() {
+    let getHand = Math.floor(Math.random() * 3);
+    return hands[getHand];
+}
 
+console.log(rockPaperScissors());
