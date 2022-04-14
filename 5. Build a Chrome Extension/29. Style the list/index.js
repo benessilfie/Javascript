dc6f43d05505f8f4820f0151abf77592757fc739
Benjamin Ofori-Quansah <bquansah007@gmail.com>
2022-04-14 07:50:14 +0000

    styling the list practice

--- 5. Build a Chrome Extension/29. Style the list/index.js
+++ 5. Build a Chrome Extension/29. Style the list/index.js
@@ -1,16 +1,16 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    renderLeads();
+});
 
 function renderLeads() {
-    let listItems = ""
+    let listItems = "";
     for (let i = 0; i < myLeads.length; i++) {
         listItems += `
             <li>
@@ -18,7 +18,7 @@ function renderLeads() {
                     ${myLeads[i]}
                 </a>
             </li>
-        `
+        `;
     }
-    ulEl.innerHTML = listItems  
+    ulEl.innerHTML = listItems;
 }
