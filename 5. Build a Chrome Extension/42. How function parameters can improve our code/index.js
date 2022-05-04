58536e21e69ca1bfc682f4b26f961edebce8f346
Benjamin Ofori-Quansah <bquansah007@gmail.com>
2022-05-04 11:59:17 +0000

    working with parameters

--- 5. Build a Chrome Extension/42. How function parameters can improve our code/index.js
+++ 5. Build a Chrome Extension/42. How function parameters can improve our code/index.js
@@ -1,18 +1,18 @@
-let myLeads = []
-let oldLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+let myLeads = [];
+let oldLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    renderLeads(myLeads)
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
 }
 
-function renderLeads(leads) {
-    let listItems = ""
+function render(leads) {
+    let listItems = "";
     for (let i = 0; i < myLeads.length; i++) {
         listItems += `
             <li>
@@ -20,20 +20,20 @@ function renderLeads(leads) {
                     ${myLeads[i]}
                 </a>
             </li>
-        `
+        `;
     }
-    ulEl.innerHTML = listItems
+    ulEl.innerHTML = listItems;
 }
 
-deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    renderLeads()
-})
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+});
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    renderLeads()
-})
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+});
