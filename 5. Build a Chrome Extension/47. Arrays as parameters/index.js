17cfd548073a3c2a155375888cb3d0b2b90483bf
Benjamin Ofori-Quansah <bquansah007@gmail.com>
2022-05-08 14:34:36 +0000

    arrays as parameters

--- 5. Build a Chrome Extension/47. Arrays as parameters/index.js
+++ 5. Build a Chrome Extension/47. Arrays as parameters/index.js
@@ -1,5 +1,10 @@
 // Create a function, getFirst(arr), that returns the first item in the array
 
-
+function getFirst(arr) {
+    return arr[1];
+}
 
 // Call it with an array as an argument to verify that it works
+let firstCard = getFirst([10, 2, 5]);
+
+console.log(firstCard);
